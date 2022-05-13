3d894a80c989ca8cd35469eeee03f8ed7cf51d3d
Corentin <plaacorentin@gmail.com>
2022-05-13 13:38:07 +0200

    ajout bloget article

---
+++ ctrl/ctrl_blog.php
@@ -0,0 +1,7 @@
+<?php
+    /* ------------------------------ IMPORT VIEW ------------------------------ */
+    include './utils/connectBDD.php';
+    include './model/model_blog.php';
+    include './view/view_blog.php'; 
+    /* ------------------------------ LOGIQUE ------------------------------ */
+?>
---
+++ view/view_blog.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="./asset/css/blog.css">
+  <title>Jeff Pizza</title>
+</head>
+
+<body style="background-color: #C5D3C5;">
+
+
+  <div class="d-flex justify-content-center m-5">
+    <h2>Blog</h2>
+  </div>
+
+
+  <div class="container">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+    
+
+      <div class="col">
+        <div class="card shadow mb-5 bg-body rounded" style="width:100%; height:500px">
+          <img src="./asset/image/pizza.jpeg"
+            class="card-img-top" alt="..." style="width:100%; height:250px">
+          <div class="card-body">
+            <h5 class="card-title">Magarita</h5>
+            <p class="card-text">Sauce tomate à l'origan, mozzarella, jambon et champignons frais</p>
+            <input id="VArt" type="submit" value="Voir Article" name="voir_article">
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col">
+      <div class="card shadow mb-5 bg-body rounded" style="width:100%; height:500px">
+          <img
+            src="./asset/image/pate.jpeg"
+            class="card-img-top" alt="..." style="width:100%; height:250px">
+          <div class="card-body">
+            <h5 class="card-title">Comment faire une bonne pate a pizza</h5>
+            <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galle</p>
+            <input id="VArt" type="submit" value="Voir Article" name="voir_article">
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col">
+      <div class="card shadow mb-5 bg-body rounded" style="width:100%; height:500px">
+          <img src="./asset/image/pastis.jpeg" class="card-img-top" alt="..." style="width:100%; height:250px">
+          <div class="card-body">
+            <h5 class="card-title">Ma pizza au Pastis</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+              content.</p>
+              <input id="VArt" type="submit" value="Voir Article" name="voir_article">
+          </div>
+          <div class="card-footer">
+            <small class="d-flex justify-content-end">
+              <a href=""><img src="./asset/image/crayon.png"></a>
+              <a href=""><img src="./asset/image/poubelle.png"></a>
+            </small>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</body>
+
+</html>
