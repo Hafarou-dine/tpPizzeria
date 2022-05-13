1b5b5319ccd65afa14666f1930ab5346f84a6d6d
vincent tordeux <vincent.tordeux@gmail.com>
2022-05-13 11:48:18 +0200

    commit vincent vendredi

---
+++ view/view_update_art.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jeff Pizza</title>
+</head>
+<body>
+<div><h1>Modifier un Article</div>
+<form>
+    <div>
+        <input type="text" name="Titre de l'article">Titre de l'article</input>
+    </div>
+    <div>
+        <input type="text" name="Contenu de l'article">Contenu
+         de l'article</input>
+    </div>
+    <div>
+        <label for="file">Ajoutez un image</label>
+        <input type="file" id="imgArt1" name="imageArt" accept="image/png, image/jpeg">
+    </div>
+    <div>
+        <label for="file">Ajoutez un image</label>
+        <input type="file" id="imgArt2" name="imageArt" accept="image/png, image/jpeg">
+    </div>
+    <div>
+        <label for="file">Ajoutez un image</label>
+        <input type="file" id="imgArt3" name="imageArt" accept="image/png, image/jpeg">
+    </div>
+</form>
+</body>
+</html>
