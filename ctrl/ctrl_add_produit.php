30725af589066db97f45583a405a9f34f6ebfcc3
Corentin <plaacorentin@gmail.com>
2022-05-13 14:05:28 +0200

    ajout modif et ajout  produit

---
+++ ctrl/ctrl_add_produit.php
@@ -0,0 +1,7 @@
+<?php
+    /* ------------------------------ IMPORT VIEW ------------------------------ */
+    include './utils/connectBDD.php';
+    include './model/model_produit.php';
+    include './view/view_add_produit.php'; 
+    /* ------------------------------ LOGIQUE ------------------------------ */
+?>
