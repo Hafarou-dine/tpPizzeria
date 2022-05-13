027ab17de806026de314ac99cb9cea494282f93c
vueling <abdel@vueling.fr>
2022-05-13 12:08:59 +0200

    initial commit

---
+++ view/view_carte.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+    <title>Jeff Pizza</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" href="#pizza">Pizza</a>
+        <a class="nav-link active" href="#pizzaCuston">Pizza Custom</a>
+        <a class="nav-link active" href="#sauce">Sauce</a>
+        <a class="nav-link active" href="#entree">Entrée</a>
+        <a class="nav-link active" href="#dessert">Déssert</a>
+        <a class="nav-link active" href="#menu">Menu</a>
+        <a class="nav-link active" href="#boisson">Boissons</a>
+    
+      </div>
+    </div>
+  </div>
+</nav>
+
+</body>
+</html>
