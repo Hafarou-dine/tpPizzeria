518847204483336e34f4bc2dbd1f988c609791c1
Hafarou-dine <hafaroupro@gmail.com>
2022-05-17 09:30:24 +0200

    blog

--- ctrl/ctrl_blog.php
+++ ctrl/ctrl_blog.php
@@ -7,56 +7,39 @@
     $liste = $all->getAllArticle($bdd);
     // on verifie si un utilisateur est connecté
     // on ouvre la div qui vacontenir les card article
-    echo '<div calss="container d-flex flex-wrap mb-5">';
+    echo '<div calss="container d-flex flex-wrap">
+    <div class="row gy-2 d-flex justify-content-evenly">';
     if(isset($_SESSION['connected'])){
         // on verifie si cet utilisateur est un Admin
         if(($_SESSION['droit'] == 1)){
             foreach($liste as $val){
                 echo '
-                <div class="card w-25">
-                <div class="card-body">
-                        <h5 class="card-title">'.$val->titre_art.'</h5>
-                        <p class="card-text">'.mb_substr($val->contenu_art, 0, 60, "UTF-8").'</p>
-                        <div class="d-flex justify-content-between">
-                            <div><a href="/tpPizzeria/article?id='.$val->id_art.'" class="btn btn-primary">Voir</a></div>
-                            <div>
-                                <a href="/tpPizzeria/updateArticle?id='.$val->id_art.'" class="btn btn-primary me-2"><img src="https://img.icons8.com/dusk/20/000000/edit--v1.png"/></a>
-                                <a href="/tpPizzeria/blog?id='.$val->id_art.'" class="btn btn-primary"><img src="https://img.icons8.com/dusk/20/000000/filled-trash.png"/></a>
+                <div class="col-sm-3 mb-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">'.$val->titre_art.'</h5>
+                            <p class="card-text">'.mb_substr($val->contenu_art, 0, 60, "UTF-8").'</p>
+                            <div class="d-flex justify-content-between">
+                                <div><a href="/tpPizzeria/article?id='.$val->id_art.'" class="btn btn-primary">Voir</a></div>
+                                <div>
+                                    <a href="/tpPizzeria/updateArticle?id='.$val->id_art.'" class="btn btn-primary me-2"><img src="https://img.icons8.com/dusk/20/000000/edit--v1.png"/></a>
+                                    <a href="/tpPizzeria/blog?id='.$val->id_art.'" class="btn btn-primary"><img src="https://img.icons8.com/dusk/20/000000/filled-trash.png"/></a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>';
-                // echo '
-                // <div class="col">
-                // <div class="card shadow mb-5 bg-body rounded" style="width:25%; height:500px">
-                //     <img src="'.$value->img_art_1.'" 
-                //     class="card-img-top" alt="..." style="width:100%; height:250px">
-                //     <div class="card-body">
-                //     <h5 class="card-title">'.$value->titre_art.'</h5>
-                //     <p class="card-text"></p>
-                //         <input id="VArt" href="/tpPizzeria/article?id='.$value->id_art.'" type="submit" value="Voir Article" name="voir_article">
-                //     </div>
-                //     <div class="card-footer">
-                //     <small class="d-flex justify-content-end">
-                //         <a href="/tpPizzeria/updateArticle?id='.$value->id_art.'"><img src="./asset/image/crayon.png"></a>
-                //         <a href=""><img src="./asset/image/poubelle.png"></a>
-                //     </small>
-                //     </div>
-                // </div>
-                // </div>';
             }
         }
         else{
-            foreach($liste as $value){
-                echo       
-                '<div class="col">
-                    <div class="card shadow mb-5 bg-body rounded" style="width:100%; height:500px">
-                        <img src="'.$value->img_art_1.'"
-                        class="card-img-top" alt="" style="width:100%; height:250px">
-                    <div class="card-body">
-                        <h5 class="card-title">'.$value->titre_art.'</h5>
-                        <p class="card-text"></p>
-                        <input id="VArt" href="/tpPizzeria/article?id='.$value->id_art.'" type="submit" value="Voir Article" name="voir_article">
+            foreach($liste as $val){
+                echo '
+                <div class="col-sm-3 mb-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">'.$val->titre_art.'</h5>
+                            <p class="card-text">'.mb_substr($val->contenu_art, 0, 60, "UTF-8").'</p>
+                            <div><a href="/tpPizzeria/article?id='.$val->id_art.'" class="btn btn-primary">Voir</a></div>
                         </div>
                     </div>
                 </div>';
@@ -64,22 +47,19 @@
         }
     }
     else{
-        foreach($liste as $value){
-            echo       
-            '<div class="col-4">
-                <div class="card shadow mb-5 bg-body rounded" style="width:100%; height:500px">
-                    <img src="'.$value->img_art_1.'"
-                    class="card-img-top" alt="..." style="width:100%; height:250px">
-                <div class="card-body">
-                    <h5 class="card-title">'.$value->titre_art.'</h5>
-                    <p class="card-text"></p>
-                    <input id="VArt" href="/tpPizzeria/article?id='.$value->id_art.'" type="submit" value="Voir Article" name="voir_article">
+        foreach($liste as $val){
+            echo '
+            <div class="col-sm-3 mb-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">'.$val->titre_art.'</h5>
+                        <p class="card-text">'.mb_substr($val->contenu_art, 0, 60, "UTF-8").'</p>
+                        <div><a href="/tpPizzeria/article?id='.$val->id_art.'" class="btn btn-primary">Voir</a></div>
                     </div>
                 </div>
             </div>';
+        }
     }
-    echo '</div>';
-}
-
+    echo '</div></div>';
 ?>
 
